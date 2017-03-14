48041ee4561d891b566d6a53fa6178df149143e9
samuraizor <andrewzinho@gmail.com>
2017-03-13 21:33:52 -0300

    FIM

---
+++ assets/boots/js/main.js
@@ -0,0 +1,35 @@
+function inicio(){
+	$(".inicio").css("display","block");
+	$(".seg").css("display","none");
+	$(".fail2").css("display","none");
+	$(".fim").css("display","none");
+	$(".fail1").css("display","none");
+}
+function mudaSim(){
+	$(".inicio").css("display","none");
+	$(".seg").css("display","block");
+	$(".fail2").css("display","none");
+	$(".fim").css("display","none");
+	$(".fail1").css("display","none");
+}
+function mudaAta(){
+	$(".inicio").css("display","none");
+	$(".seg").css("display","none");
+	$(".fail2").css("display","none");
+	$(".fim").css("display","none");
+	$(".fail1").css("display","block");
+}
+function mudaShow(){
+	$(".inicio").css("display","none");
+	$(".seg").css("display","none");
+	$(".fail2").css("display","none");
+	$(".fim").css("display","block");
+	$(".fail1").css("display","none");
+}
+function mudaFon(){
+	$(".inicio").css("display","none");
+	$(".seg").css("display","none");
+	$(".fail2").css("display","block");
+	$(".fim").css("display","none");
+	$(".fail1").css("display","none");
+}
