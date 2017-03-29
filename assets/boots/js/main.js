1fe484486239417a538a48253075198f83d4e7bd
samuraizor <andrewzinho@gmail.com>
2017-03-29 23:28:16 +0100

    geo2

--- assets/boots/js/main.js
+++ assets/boots/js/main.js
@@ -33,12 +33,12 @@ function mudaFon(){
 	$(".fim").css("display","none");
 	$(".fail1").css("display","none");
 }
-var x = document.getElementById("demo");
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        $("#demo").html("Geolocation is not supported by this browser.");
     }
 }
 function showPosition(position) {
