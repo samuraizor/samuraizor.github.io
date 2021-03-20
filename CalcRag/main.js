cfe1b5a4c3c5a7931771d7804e162ad4e98f6df2
samuraizor <andrewzinho@gmail.com>
2021-03-20 04:15:11 +0000

    Criação visual

--- CalcRag/main.js
+++ CalcRag/main.js
@@ -1,8 +1,20 @@
-var dex=1, luk=1, str=1
+var dex=1, luk=3, str=5
 var pgD=0, pgL=0, pgS=0
+var dexMax=false, strMax=false, lukMax=false;
+function btnCalcLvl(){
+	var level = 0;
+	level = $("#level").val();
+	getFullPontos(level)
+}
+
+function btnCalcPts(){
+	var pontos = 0;
+	pontos = $("#pontos").val();
+	distribuiRanged(pontos)
+}
+
 
 function verificaPontos(lvl){
-	//=SE(P4<=100;ARREDONDAR.PARA.CIMA(P4/10;0)+1;ARREDONDAR.PARA.CIMA((P4-100)/5;0)*4+12)
 	if (lvl<100){
 		return Math.ceil(lvl/10)+1
 	}else{
@@ -10,7 +22,6 @@ function verificaPontos(lvl){
 	}
 }
 function pontosLevel(lvl){
-	//=SE(lvl<100;lvl/5+2;round((lvl-100)/10)+22)
 	if (lvl<100){
 		return Math.ceil(lvl/5)+2
 	}else{
@@ -19,10 +30,9 @@ function pontosLevel(lvl){
 }
 
 function getFullPontos(level){
-	var pontos=100;
+	var pontos=88;
 	for(i=2;i<=level;i++){		
 		pontos = pontos + pontosLevel(i);
-		//console.log("Level "+i+": "+pontos+" - SOMOU:"+pontosLevel(i))
 	}
 	console.log("Pontos Total: " + pontos)	
 	distribuiRanged(pontos)
@@ -31,31 +41,32 @@ function getFullPontos(level){
 function distribuiRanged(pontos){
 	var aux;
 	aux = pontos;
-	//1 dex = 3 luk = 5 str
 	while (pontos>=aux)	{
 		console.log("Sobrou +" + aux + " pontos.")
 		var nDex=0,nLuk=0, nStr=0;
 		nDex=verificaPontos(dex+1)
 		nLuk = verificaPontos(luk+1) + verificaPontos(luk+2) + verificaPontos(luk+3)
 		nStr = verificaPontos(str+1) + verificaPontos(str+2) + verificaPontos(str+3) + verificaPontos(str+4) +verificaPontos(str+5)
-		if (nDex <= nLuk){
+		if (nDex <= nLuk && !dexMax){
 			if (nDex<=aux){
 				addDex();
 				aux = aux - nDex;
 				pgD = pgD + nDex;
 			}else{break;}
-		}else if(nLuk <=nStr) {
+		}else if(nLuk <=nStr && !lukMax) {
 			if (nLuk<=aux){
 				addLuk();
 				aux = aux - nLuk;
 				pgL = pgL + nLuk;
 			}else{break;}
-		}else{
+		}else if (!strMax){
 			if (nStr<=aux){
 				addStr();
 				aux = aux - nStr;
 				pgS = pgS + nStr;
 			}else{break;}
+		}else{
+			break;
 		}
 	}
 	
@@ -63,14 +74,34 @@ function distribuiRanged(pontos){
 	
 }
 
-function addDex(){dex++;}
-function addLuk(){luk+=3;}
-function addStr(){str+=5;}
+function addDex(){
+	dex++;
+	if (dex>=125){dexMax=true}
+}
+function addLuk(){
+	luk+=3;
+	if (luk>=122){lukMax=true}
+}
+function addStr(){
+	str+=5;
+	if (str>=120){strMax=true}
+}
 
 function Escreve(){
-	console.log('Dex: '+dex+' Luk: '+luk+' Str: '+str);
+	$(".str").html(str);
+	$(".dex").html(dex);
+	$(".luk").html(luk);
 	console.log('Pontos gastos')
 	console.log('Dex: '+pgD)
 	console.log('Luk: '+pgL)
 	console.log('Str: '+pgS);
+dex=1;
+luk=3;
+str=5;
+pgD=0;
+pgL=0;
+pgS=0;
+dexMax=false;
+strMax=false;
+lukMax=false;
 }
